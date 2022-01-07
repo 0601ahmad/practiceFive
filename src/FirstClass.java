0056091a328669f687b855be902885e9879574b2
0601ahmad <97071739+0601ahmad@users.noreply.github.com>
2022-01-07 10:33:00 -0800

    Update FirstClass.java

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -4,6 +4,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("This is another practice");
 		System.out.println("This is not another one bro");
+		System.out.println("This is another change");
 
 	}
 
